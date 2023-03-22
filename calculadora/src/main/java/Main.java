3cd80dd13c81fee6436e52282752caa20552105e
JErnestoHc <julmaster15@gmail.com>
2023-03-22 17:59:36 -0600

    agregando implementacion

--- calculadora/src/main/java/Main.java
+++ calculadora/src/main/java/Main.java
@@ -1,5 +1,9 @@
+import calculadora.Calculadora;
+import operaciones.Suma;
+
 public class Main {
     public static void main(String[] args) {
-
+        Calculadora calculadora = new Calculadora(new Suma());
+        System.out.println(calculadora.calcular(2,5));
     }
 }
