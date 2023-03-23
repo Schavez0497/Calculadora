d3da10822d2008f2700a6d7094b3a68e2d0f2d63
linkop <guiurbina@gmail.com>
2023-03-22 18:05:34 -0600

    menu

--- calculadora/src/main/java/Main.java
+++ calculadora/src/main/java/Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Scanner scannerString=new Scanner(System.in);
 
+
+        int opcion =0;
+
+        while (true){
+
+            mostrarMenu();
+            opcion=scanner.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Sumar:");
+                    break;
+                case 2:
+                    System.out.println("Resta");
+                    break;
+                case 3:
+                    System.out.println("multi");
+                    break;
+                case 4:
+                    System.out.println("division");
+                default:
+                    break;
+
+            }
+
+            String salir="";
+            System.out.println("Desea Continuar (S/N:)");
+            salir= scannerString.nextLine();
+
+            if (!salir.equalsIgnoreCase("S")){
+                System.exit(0);
+            }
+        }
+    }
+    public static void mostrarMenu(){
+        System.out.println("******************************");
+        System.out.println("Menu Calculadora");
+        System.out.println("*******************************");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicacion");
+        System.out.println("4. Division");
+        System.out.println("*******************************");
     }
 }
