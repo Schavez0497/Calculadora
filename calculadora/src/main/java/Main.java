987c859b25a8f97f0d1e487e039c7bc48a238fd0
franksj1997 <fesj700@hotmail.com>
2023-03-23 16:48:20 -0600

    super cambio xd

--- calculadora/src/main/java/Main.java
+++ calculadora/src/main/java/Main.java
@@ -1,10 +1,19 @@
 
+import operaciones.*;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Scanner scannerint = new Scanner(System.in);
         Scanner scannerString=new Scanner(System.in);
+        ICalculo multiplicar = new Multiplicacion();
+        ICalculo restar = new Resta();
+        ICalculo sumar = new Suma();
+        ICalculo dividir = new Division();
+        int a = 0, b = 0;
+
 
         int opcion =0;
 
@@ -16,15 +25,35 @@ public class Main {
             switch (opcion){
                 case 1:
                     System.out.println("Sumar:");
+                    System.out.println("Ingrese el Primer Numero: ");
+                    a = scannerint.nextInt();
+                    System.out.println("Ingrese el segundo Numero: ");
+                    b = scannerint.nextInt();
+                    System.out.println("La suma es: " +sumar.calcular(a,b));
                     break;
                 case 2:
                     System.out.println("Resta");
+                    System.out.println("Ingrese el Primer Numero: ");
+                    a = scannerint.nextInt();
+                    System.out.println("Ingrese el segundo Numero: ");
+                    b = scannerint.nextInt();
+                    System.out.println("La Resta es: " + restar.calcular(a,b));
                     break;
                 case 3:
                     System.out.println("multi");
+                    System.out.println("Ingrese el Primer Numero: ");
+                    a = scannerint.nextInt();
+                    System.out.println("Ingrese el segundo Numero: ");
+                    b = scannerint.nextInt();
+                    System.out.println("La Multiplicacion es: " + multiplicar.calcular(a,b));
                     break;
                 case 4:
                     System.out.println("division");
+                    System.out.println("Ingrese el Primer Numero: ");
+                    a = scannerint.nextInt();
+                    System.out.println("Ingrese el segundo Numero: ");
+                    b = scannerint.nextInt();
+                    System.out.println("La Division es: " + dividir.calcular(a,b));
                 default:
                     break;
 
